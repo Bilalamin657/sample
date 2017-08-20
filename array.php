86e0394c4586559a8cbc54c1fc4ab81a05f24d75
bilalamin657 <bilalamin657@gmail.com>
2017-08-20 14:11:30 -0700

    i have added a new file array.php

---
+++ array.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<title>ARRAYS IN PHP</title>
+</head>
+<body>
+<?php
+
+$arrayvariable = array("Karachi is in Pakistan","Mumbai is in INDIA","Newyork is in America");
+echo "<b>where is karachi..??</b><br>" . $arrayvariable[0] . "<br>";
+echo "<b>where is Mumbai..??</b><br>" . $arrayvariable[1] . "<br>";
+echo "<b>where is Newyork..??</b><br>" . $arrayvariable[2] . "<br>";
+
+?>
+</body>
+</html>
